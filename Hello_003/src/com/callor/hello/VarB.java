0f735df8c0a17b51bbfba298815ba67ebf3342f1
unknown <ckw2434@naver.com>
2023-11-23 11:02:03 +0900

    변수추가

---
+++ Hello_003/src/com/callor/hello/VarB.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+public class VarB {
+	public static void main(String[] args) {
+		
+		int num = 0; // 기본 정수형 변수 선언
+		long num2 = 0; // 큰 정수값을 보관하는 변수 선언
+		
+		double num3 = 0; // 기본 실수형 변수 선언
+		float num4 = 0; // 작은 실수값을 보관하는 변수 선언
+	}
+
+}
