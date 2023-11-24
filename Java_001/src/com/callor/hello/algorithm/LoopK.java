7cc0afc66241cc72f151e9420d19ab5bb989ae27
unknown <ckw2434@naver.com>
2023-11-24 16:32:04 +0900

    루프 활용

--- Java_001/src/com/callor/hello/algorithm/LoopK.java
+++ Java_001/src/com/callor/hello/algorithm/LoopK.java
@@ -2,6 +2,18 @@ package com.callor.hello.algorithm;
 
 public class LoopK {
 	public static void main(String[] args) {
+		int sum = 0;
+		int range = 0;
+		range = 37;
+		for(int i = 0; i < range ; i++) {
+			
+			sum += i;
+			
+		}
+		System.out.println("==================");
+		System.out.printf("1 ~ %d 덧셈결과 : ", range);
+		System.out.println(sum);
+		System.out.println("==================");
 		
 	}
 
