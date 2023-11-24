9c96820da218631027555c5c9d154045ecf3c60e
unknown <ckw2434@naver.com>
2023-11-24 11:52:01 +0900

    알고리즘

---
+++ Java_001/src/com/callor/hello/pay/PayC.java
@@ -0,0 +1,40 @@
+package com.callor.hello.pay;
+
+public class PayC {
+	public static void main(String[] args) {
+		int pay = 3587960;
+		
+		System.out.println("====================");
+		System.out.println("5 만원권 : " + pay / 50000);
+		
+		int num = pay % 50000;
+		pay = num;
+		
+		System.out.println("1 만원권 : " + pay /10000);
+		num = pay % 10000; // 1만원 단위 제거
+		pay = num;
+		
+		System.out.println("5 천원권 : " + pay / 5000 );
+		num = pay % 5000; // 5천원 단위 제거
+		pay = num;
+		
+		System.out.println("5 백원권 : " + pay / 500 );
+		num = pay % 500; // 5백원 단위 제거
+		pay = num;
+		
+		System.out.println("1 백원권 : " + pay / 100 );
+		num = pay % 100; //  1백원 단위 제거
+		pay = num;
+		
+		System.out.println("50 원권 : " + pay / 50 );
+		num = pay % 50; // 50원 단위 제거
+		pay = num;
+			
+	
+	}
+	
+
+
+	
+
+}
