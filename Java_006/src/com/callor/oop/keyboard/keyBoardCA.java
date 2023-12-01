286a3a7b43f61d40a4bca9b84aab30da2566b350
unknown <ckw2434@naver.com>
2023-12-01 16:39:56 +0900

    KeyBroad

--- Java_006/src/com/callor/oop/keyboard/keyBoardCA.java
+++ Java_006/src/com/callor/oop/keyboard/keyBoardCA.java
@@ -6,10 +6,10 @@ public class keyBoardCA {
 	
 	public static void main(String[] args) {
 		
-		while(true) {
 		Scanner scan = new Scanner(System.in);
+		while(true) {
 		int rndNum = (int)(Math.random()* 50)+ 1;
-		int count = 1;
+		int count = 0;
 		
 		System.out.println("=".repeat(50));
 		System.out.println("숫자 맞추기 게임");
@@ -54,10 +54,16 @@ public class keyBoardCA {
 			System.out.println("다시 한번 잘 생각해보세요. ");
 		}while(true) {
 			System.out.print("X 또는 O ");
-			System.out.println("");
-			break;
+			String str = scan.nextLine();
+			if(str.equals("O")) {
+				break;
+				
+			}if(str.equals("X")) {
+				System.out.println("종료합니다. ");
+				return;
+			}
 		}
-		System.out.println("GAME OVER!!");
+		
 		continue;
 		
 	}
