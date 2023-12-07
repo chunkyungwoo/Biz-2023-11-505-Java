a04452ce679257bd4da3e0b839a7419334b7494a
unknown <ckw2434@naver.com>
2023-12-07 16:07:04 +0900

    List

---
+++ Java_010/src/com/callor/score/exec/ScoreC.java
@@ -0,0 +1,33 @@
+package com.callor.score.exec;
+
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.NumberService;
+import com.callor.score.utils.Line;
+
+public class ScoreC {
+	
+	public static void main(String[] args) {
+		NumberService numService = new NumberService();
+		ScoreDto scoreDto = new ScoreDto();		
+		int start = 50;
+		int end = 100;
+		Line.dLine(50);
+		System.out.println("성적 리스트");
+		Line.dLine(50);
+		System.out.println("국어\t영어\t수학\t총점\n");
+		Line.sLine(50);
+		int scoreKor = numService.inputNumber("국어 점수",start,end);
+	    int scoreEng = numService.inputNumber("영어 점수",start,end);
+	    int scoreMath = numService.inputNumber("수학 점수",start,end);
+		scoreDto.kor = scoreKor;
+		scoreDto.eng = scoreEng;
+		scoreDto.math = scoreMath;
+		
+		System.out.printf("총점"+scoreDto.getTotal());
+		for(int i = 0; i < 3; i++) {
+			
+		}
+		
+	}
+
+}
---
+++ Java_010/src/com/callor/score/exec/ScoreD.java
@@ -0,0 +1,41 @@
+package com.callor.score.exec;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.NumberService;
+import com.callor.score.utils.Line;
+
+public class ScoreD {
+	
+	public static void main(String[] args) {
+		NumberService numService = new NumberService();
+		List<ScoreDto> scores = new ArrayList<ScoreDto>();
+		
+		int start = 50;
+		int end = 100;
+		
+		for(int index = 0; index < 3; index++) {
+			int stdNum = index + 1;
+			String stNum = String.format("S%04d", index+1);
+			int scoreKor = numService.inputNumber(stdNum + "번의 국어 점수",start,end);
+			int scoreEng = numService.inputNumber(stdNum + "번의 영어 점수",start,end);
+			int scoreMath = numService.inputNumber(stdNum + "번의 수학 점수",start,end);
+					
+			ScoreDto scoreDto = new ScoreDto();
+			scoreDto.kor = scoreKor;
+			scoreDto.eng = scoreEng;
+			scoreDto.math = scoreMath;
+			scores.add(scoreDto);
+		}
+		Line.dLine(50);
+		System.out.println("성적 리스트");
+		Line.dLine(50);
+		System.out.println("학번\t국어\t영어\t수학\t총점");
+		
+			
+		
+	}
+
+}
