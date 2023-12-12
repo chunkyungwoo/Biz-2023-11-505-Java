6031f476c701d6087949d9df6555b7439b305717
unknown <ckw2434@naver.com>
2023-12-12 16:25:08 +0900

    수정

--- Java_004/src/com/callor/hello/arrays/ArraysB.java
+++ Java_004/src/com/callor/hello/arrays/ArraysB.java
@@ -4,15 +4,21 @@ public class ArraysB {
 	
 	public static void main(String[] args) {
 		
-		int index = 0;
-		int scoreKors = 10;
-		int[] nums = new int[scoreKors];
-		for (int i = 0; i < 10; i++) {
-			int rndNum = (int)(Math.random()*50)+ 51;
-			nums[i] = rndNum;
-		}
-		System.out.printf("");
+		int STUDENT_LENGTH = 10;
+		int[] scoreKors = new int[STUDENT_LENGTH];
 		
+		for (int i = 0; i < scoreKors.length; i++) {
+			int rndscore = (int)(Math.random()*50)+ 51;
+			scoreKors[i] = rndscore;
+		}
+		int sum = 0;
+		for(int i = 0; i < scoreKors.length; i++) {
+			sum += scoreKors[i];
+		}
+		float avg = 0;
+		avg = (float)sum / scoreKors.length;
+		System.out.println("=".repeat(50));
+		System.out.printf("국어 점수 평균 : %d 평균 %.2f\n",sum,avg);
 	}
 
 }
